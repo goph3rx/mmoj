1759b4ffe5e59e470d02882722f84b610f9a452f
goph3r <82804585+goph3rx@users.noreply.github.com>
2022-12-24 19:57:23 +0000

    Dependency injection for the auth server

---
+++ auth/src/main/java/com/github/goph3rx/auth/AuthComponent.java
@@ -0,0 +1,38 @@
+package com.github.goph3rx.auth;
+
+import com.github.goph3rx.account.AccountModule;
+import com.github.goph3rx.transfer.ITransferService;
+import com.github.goph3rx.transfer.TransferModule;
+import com.github.goph3rx.world.WorldModule;
+import dagger.Component;
+import javax.inject.Singleton;
+
+/** Main component for the auth server. */
+@Singleton
+@Component(
+    modules = {AuthModule.class, AccountModule.class, TransferModule.class, WorldModule.class})
+public interface AuthComponent {
+  /**
+   * Inject dependencies into the client.
+   *
+   * @param client Client.
+   */
+  void injectClient(AuthClient client);
+
+  /**
+   * Inject dependencies into the connection.
+   *
+   * @param connection Connection.
+   */
+  void injectConnection(AuthConnection connection);
+
+  /**
+   * Inject dependencies into the server.
+   *
+   * @param server Server.
+   */
+  void injectServer(AuthServer server);
+
+  /** Get the service for world transfers */
+  ITransferService transfers();
+}
