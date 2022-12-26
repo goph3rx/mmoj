10f68476e89045588ca99afc5446045c0e069582
goph3r <82804585+goph3rx@users.noreply.github.com>
2022-12-26 20:24:35 +0000

    Logging for client manager on the auth server

--- auth/src/main/java/com/github/goph3rx/auth/AuthClientManager.java
+++ auth/src/main/java/com/github/goph3rx/auth/AuthClientManager.java
@@ -3,9 +3,14 @@ package com.github.goph3rx.auth;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.concurrent.locks.ReentrantLock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Manager for clients connected to the server. */
 public class AuthClientManager implements IAuthClientManager {
+  /** Logger for this class. */
+  private static final Logger logger = LoggerFactory.getLogger(AuthClientManager.class);
+
   /** List of clients connected to the server. */
   private final LinkedList<AuthClient> clients = new LinkedList<>();
   /** Lock for synchronizing access. */
@@ -13,19 +18,25 @@ public class AuthClientManager implements IAuthClientManager {
 
   @Override
   public void add(AuthClient client) {
+    logger.debug("Adding client {}", client);
     lock.lock();
     try {
       clients.add(client);
     } finally {
       lock.unlock();
     }
+    logger.debug("Success");
   }
 
   @Override
   public Optional<AuthClient> find(String username) {
+    logger.debug("Finding clients username='{}'", username);
     lock.lock();
     try {
-      return clients.stream().filter(client -> client.getUsername().equals(username)).findFirst();
+      var result =
+          clients.stream().filter(client -> client.getUsername().equals(username)).findFirst();
+      logger.debug("Result is {}", result);
+      return result;
     } finally {
       lock.unlock();
     }
@@ -33,11 +44,13 @@ public class AuthClientManager implements IAuthClientManager {
 
   @Override
   public void remove(AuthClient client) {
+    logger.debug("Removing client {}", client);
     lock.lock();
     try {
       clients.remove(client);
     } finally {
       lock.unlock();
     }
+    logger.debug("Success");
   }
 }
