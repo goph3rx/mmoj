e225c398836acc60b8a58233ecc35d24471d684a
goph3rx <82804585+goph3rx@users.noreply.github.com>
2022-12-28 08:31:54 +0000

    Update world statuses (#31)

--- common/src/main/java/com/github/goph3rx/world/IWorldDatabase.java
+++ common/src/main/java/com/github/goph3rx/world/IWorldDatabase.java
@@ -6,4 +6,18 @@ import java.util.List;
 public interface IWorldDatabase {
   /** Fetch the list of all worlds. */
   List<World> list();
+
+  /**
+   * Save the status of the game world.
+   *
+   * @param world World status.
+   */
+  void save(World world);
+
+  /**
+   * Set worlds to offline in case of no recent updates.
+   *
+   * @return Total number of worlds marked offline.
+   */
+  int updateOffline();
 }
